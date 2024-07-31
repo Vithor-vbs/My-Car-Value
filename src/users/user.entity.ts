2ce80c8c24264ddf6a75c9b238e83fb646a754c0
Vithor-vbs <vithorvilas12@gmail.com>
2024-07-31 16:36:27 -0300

    handling e2e tests and startup envs

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -5,6 +5,7 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
+  OneToMany,
 } from 'typeorm';
 
 @Entity()
@@ -17,6 +18,9 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[]
 
   @AfterInsert()
   logInsert() {
