aefc096d31e7b46a9ddfb492530114f1ea30b2f0
Vithor-vbs <vithorvilas12@gmail.com>
2024-08-01 20:00:08 -0300

    adding getEstimate feature

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,23 +4,39 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { User } from 'src/users/user.entity';
+import { getEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
-    constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-    create(reportDto: CreateReportDto, user: User){
-        const report = this.repo.create(reportDto)
-        report.user = user
-        return this.repo.save(report)
-    }
+  create(reportDto: CreateReportDto, user: User) {
+    const report = this.repo.create(reportDto);
+    report.user = user;
+    return this.repo.save(report);
+  }
 
-    async changeApproval(id: string, approved: boolean) {
-        const report = await this.repo.findOne({ where: { id: parseInt(id) } });
-        if (!report) throw new NotFoundException('report not found')
-        
-        report.approved = approved
-        return this.repo.save(report)
-     
-      }
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repo.findOne({ where: { id: parseInt(id) } });
+    if (!report) throw new NotFoundException('report not found');
+
+    report.approved = approved;
+    return this.repo.save(report);
+  }
+
+  createEstimate({ make, model, lng, lat, mileage, year }: getEstimateDto) {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', { make: make }) //avoid sql injection
+      .andWhere('model = :model', { model: model })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .andWhere('approved IS TRUE')
+      .orderBy('ABS(mileage -:mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
+  }
 }
